6ce3f8c4e68d542c2e6d15fb92912823390094ee
Krishan Madushanka <krishan@ideabits.se>
2022-12-30 19:45:52 +0530

    Initial Commit

---
+++ HealthkitIntegration/HealthkitIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthkitIntegrationApp.swift
+//  HealthkitIntegration
+//
+//  Created by Krishan Madushanka on 2022-12-30.
+//
+
+import SwiftUI
+
+@main
+struct HealthkitIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
